7030b812a98d6baed62cad24107340b20f3ffa56
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2023-03-03 09:56:24 +0100

    Update HelloWord.java

--- HelloWord.java
+++ HelloWord.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-	public static void main(String[] args) {
+	public static void main(String[] args) { //creato in automatico il metodo main (se hai selezionato nel menu)
 		// TODO Auto-generated method stub
 		System.out.println("Ciao mondo!");
 	}
